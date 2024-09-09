f6ac3607f3d73544abe39e24b7cae5149ea0e798
Ayu5hsingh <heyayushsingh05@gmail.com>
2024-09-09 20:19:36 +0530

    added user signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,55 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import {z} from "zod";
+import { useTransition } from "hono/jsx";
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string
+	}
+}>();
 
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-
-const prisma = new PrismaClient({
-    datasourceUrl: env.DATABASE_URL,
-}).$extends(withAccelerate())
-
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const SignUpBodySchema = z.object({
+  name: z.string().optional(),
+  email: z.string().email(),
+  password: z.string().min(8)
 })
 
+type UserSchema = z.infer<typeof SignUpBodySchema>;
+interface env {
+  DATABASE_URL: string;
+}
 
-app.post('/api/v1/user/signup',(c)=>{
-  return c.text('User registered successfully!')
-})
 
-app.post('/api/v1/user/signin',(c)=>{
-  return c.json({message: 'User logged in successfully!', token: 'your_token'})
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.post('/api/v1/blog',(c)=>{
-  return c.json({message: 'Blog created successfully!', id: 'your_id'})
-})
+app.post("/api/v1/user/signup", async (c) => {
+  
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+  
+  const user: UserSchema = SignUpBodySchema.parse( await c.req.json());
+  console.log("User registered:", user);
+  return c.text("User registered successfully!");
+});
 
-app.put('/api/v1/blog/:id',(c)=>{
-  return c.json({message: 'Blog updated successfully!'})
-})
+app.post("/api/v1/user/signin", (c) => {
+  return c.json({
+    message: "User logged in successfully!",
+    token: "your_token",
+  });
+});
 
-export default app
+app.post("/api/v1/blog", (c) => {
+  return c.json({ message: "Blog created successfully!", id: "your_id" });
+});
+
+app.put("/api/v1/blog/:id", (c) => {
+  return c.json({ message: "Blog updated successfully!" });
+});
+
+export default app;
